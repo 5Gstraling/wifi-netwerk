079cb05cab591a2a9419ff425a0a49e598963c6d
lisadv <78847178+lisadv@users.noreply.github.com>
2021-05-12 09:52:29 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,12 @@
 
 //#include <Wire.h>
 
-
 // Set your access point network credentials
 const char* ssid = "stralingslocatie";
 const char* password = "123456789";
 
-
 // Create AsyncWebServer object on port 80
 //AsyncWebServer server(80);
-
 
 void setup(){
   // Serial port for debugging purposes
@@ -19,16 +16,15 @@ void setup(){
   Serial.println();
   
   // Setting the ESP as an access point
+  WiFi.mode(WIFI_AP);
   Serial.print("Setting AP (Access Point)…");
   // Remove the password parameter, if you want the AP (Access Point) to be open
-  WiFi.softAP(ssid, password);
+  WiFi.softAP(ssid, password, 8);
 
   IPAddress IP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(IP);
-  
 }
  
 void loop(){
-  
 }
